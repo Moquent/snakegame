236e76428fc89e794cf3d86ad1c75cd47eabb9cf
TejeshCodes <63721912+TejeshCodes@users.noreply.github.com>
2020-06-09 09:52:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var snake;
+var food;
+var w, h;
+var scl = 15;
+var a, b, c, d;
+
+function setup() {
+  createCanvas(400, 400);
+
+
+  w = floor(width / scl);
+  h = floor(width / scl);
+
+  snake = new Snake();
+
+  foodLocation();
+
+  frameRate(5);
+}
+
+function draw() {
+  scale(scl);
+  background("yellow");
+
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+
+  snake.update();
+  snake.display();
+
+  if (snake.endGame()) {
+    background("black");
+    console.log("GAME OVER");
+    noLoop();
+  }
+
+  fill("red");
+  rect(food.x, food.y, 1, 1);
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW && snake.ydir != 1) {
+    snake.setDir(0, -1);
+  } else if (keyCode == DOWN_ARROW && snake.ydir != -1) {
+    snake.setDir(0, 1);
+  } else if (keyCode == RIGHT_ARROW && snake.xdir != -1) {
+    snake.setDir(1, 0);
+  } else if (keyCode == LEFT_ARROW && snake.xdir != 1) {
+    snake.setDir(-1, 0);
+  }
+}
+
+function foodLocation() {
+  var x = floor(random(h));
+  var y = floor(random(w));
+  food = createVector(x, y);
+}
